05a06e70405cc9f45add53c10af5c9aeeec21f55
Artiom Astashonak <artiomastashonak@gmail.com>
2025-09-08 15:40:05 +0200

    Add docs to `useForm` composable.

--- app/composables/useForm.ts
+++ app/composables/useForm.ts
@@ -1,5 +1,14 @@
 type FormFields = Record<string, string>
 
+/**
+ * useForm composable. Designed to abstract the form validation and submition.
+ * 
+ * @param {FormFields} init key-value pair of the name of the form field and its value
+ * @param {(form: FormFields) => string} validateFn validation function that's run when
+ * the form is submitted
+ * @returns `form` javascript object, `error` containing stringified error value
+ * and `submitForm` function
+ */
 export function useForm(init: FormFields, validateFn: (form: FormFields) => string) {
   const form = reactive({ ...init });
   const error = ref("");
