2360739b23e09bc1421dfaebbe1817a7ecc33d22
Artiom Astashonak <artiomastashonak@gmail.com>
2025-09-28 12:45:02 +0200

    Store currently logged in user on the frontend.

---
+++ app/composables/useMe.ts
@@ -0,0 +1,27 @@
+import type { User } from "#shared/types/api/auth";
+
+// Nuxt composables must be synchoronous, otherwise they can't
+// function properly. Thus, we need to create an async wrapper
+// `fetchMe` to asynchronously fetch userdata but still remain
+// in sync context.
+
+/**
+ * Gets currently logged in user data with by making an API
+ * call to `/api/auth/me`.
+ * 
+ * @returns logged in user
+ */
+export function useMe() {
+  const me = useState<User | null>("me", () => null);
+
+  const fetchMe = async () => {
+    try {
+      me.value = await $fetch("/api/auth/me");
+    } catch (e) {
+      me.value = null;
+    }
+  }
+  fetchMe()
+
+  return me;
+}
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { useMe } from '~/composables/useMe';
+
 useSeoMeta({
   title: "Wealth Tracker",
   description: "See your financial history and plan your future on this website!"
@@ -8,9 +10,12 @@ async function logout() {
   await $fetch("/api/auth/logout", { method: "POST" });
   location.reload()
 }
+
+const me = useMe();
 </script>
 
 <template>
   <h1>hello world</h1>
+  <p>{{ me }}</p>
   <AppButton @click="logout">Logout</AppButton>
 </template>
